370c284f43587c9ca0e9671c5bdc0ab1e1eed89a
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-10-30 18:31:54 +0200

    Complete lesson 'Creating Variables with Types'

---
+++ csharp/data_types_and_variables/CreatingVariablesWithTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingVariablesWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Variables
+            string name = "Shadow";
+            string breed = "Golden Retriever";
+            int age = 5;
+            double weight = 65.22;
+            bool spayed = true;
+
+            // Print variables to the console
+            Console.WriteLine(name);
+            Console.WriteLine(breed);
+            Console.WriteLine(age);
+            Console.WriteLine(weight);
+            Console.WriteLine(spayed);
+        }
+    }
+}
