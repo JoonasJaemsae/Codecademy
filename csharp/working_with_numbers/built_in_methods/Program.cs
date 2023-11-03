9516364d669c188ec6efe250cb139b782794bd12
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-03 13:38:17 +0200

    Add exercise 'Built-in Methods'

---
+++ csharp/working_with_numbers/built_in_methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starting variables 
+            int numberOne = 12932;
+            int numberTwo = -2828472;
+
+            // Use built-in methods and save to variable 
+            double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+
+            // Use built-in methods and save to variable 
+            double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+
+            // Print the lowest number
+            Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+        }
+    }
+}
+
