3cbab2d225a1cee2ae6e797934bbdc4eb2e48e2e
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-04 11:02:25 +0200

    Complete 'Review' or 'Working with Text'

---
+++ csharp/working_with_text/Review/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            The following program builds a new string from an existing string and randomly makes each character either uppercase or lowercase and then outputs the new string.
+            */
+            string randomcaseString = "The characters in this string are randomly uppercase or lowercase";
+
+            var sb = new System.Text.StringBuilder();
+            int length = randomcaseString.Length;
+            int i = 0;
+            string oneChar;
+            Random rnd = new Random();
+            int num;
+            while (i < length)
+            {
+                num = rnd.Next(0, 2);
+                if (num % 2 == 0)
+                {
+                    oneChar = randomcaseString.Substring(i, 1).ToUpper();
+                    sb.Append(oneChar);
+                }
+                else
+                {
+                    oneChar = randomcaseString.Substring(i, 1).ToLower();
+                    sb.Append(oneChar);
+                }
+                i++;
+            }
+            Console.WriteLine($"{sb}.");
+            Console.ReadLine();
+        }
+    }
+}
