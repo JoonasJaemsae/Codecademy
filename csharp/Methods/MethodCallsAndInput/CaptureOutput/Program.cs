e2f65ac4d7cbf6b7f543ba460a210ac50bec06f0
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-06 12:12:02 +0200

    Complete exercise 'CaptureOutput'

---
+++ csharp/Methods/MethodCallsAndInput/CaptureOutput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptureOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string designer = "Anders Hejlsberg";
+            int indexOfSpace = designer.IndexOf(" ");
+            string secondName = designer.Substring(indexOfSpace + 1, 9);
+            Console.WriteLine(secondName);
+            Console.ReadLine();
+        }
+    }
+}
