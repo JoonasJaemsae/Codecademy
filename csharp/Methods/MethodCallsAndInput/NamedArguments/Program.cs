2e78d7e2b0ce3a92b0c2eb9b0db880ebbdbafbeb
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-06 12:34:50 +0200

    Complete exercise 'NamedArguments'

---
+++ csharp/Methods/MethodCallsAndInput/NamedArguments/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VisitPlanets(numberOfPlanets: 2, name: "Joonas");
+            Console.ReadLine();
+        }
+
+        static void VisitPlanets(
+          string adjective = "brave",
+          string name = "Cosmonaut",
+          int numberOfPlanets = 0,
+          double gForce = 4.2)
+        {
+            Console.WriteLine($"Welcome back, {adjective} {name}.");
+            Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+            Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+        }
+    }
+}
