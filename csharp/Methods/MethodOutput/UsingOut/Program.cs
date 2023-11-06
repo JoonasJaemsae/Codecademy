11cc21f1bcbfaa7391999fcb60989115087f7f2b
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-06 14:17:16 +0200

    Complete exercise 'UsingOut'

---
+++ csharp/Methods/MethodOutput/UsingOut/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string statement = "GARRRR";
+            Console.WriteLine(Whisper(statement, out bool marker));
+            Console.WriteLine(marker);
+            Console.ReadLine();
+        }
+
+        static string Whisper(string param1, out bool param2)
+        {
+            param2 = true;
+            return param1.ToLower();
+        }
+    }
+}
