23b4a274cd0a54296b5a1bc20dcaef3da0b50442
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-07 20:01:03 +0200

    Complete exercise 'BasicClasses'

---
+++ csharp/6_ClassesAndObjects/BasicClassesAndObjects/BasicClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest("Congo", "Tropical");
+            f.Trees = 0;
+
+            Forest f2 = new Forest("Rendlesham");
+            Console.WriteLine(f2.Biome);
+
+            Console.WriteLine(f.Name);
+            Console.WriteLine(f.Biome);
+            Console.ReadLine();
+        }
+    }
+}
