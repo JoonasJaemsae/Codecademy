97268c7b50c84a539ad8b1135f69ee0e72813a41
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-07 12:38:15 +0200

    "Complete exercise 'LambdaExpressions'"

---
+++ csharp/Methods/AlternateExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] spaceRocks = { "meteoroid", "meteor", "meteorite" };
+
+            bool makesContact = Array.Exists(spaceRocks, (string s) => s == "meteorite");
+
+            if (makesContact)
+            {
+                Console.WriteLine("At least one space rock has reached the Earth's surface!");
+            }
+            Console.ReadLine();
+        }
+
+        // Replaced with a lambda expression.
+        static bool HitGround(string s)
+        {
+            return s == "meteorite";
+        }
+    }
+}
