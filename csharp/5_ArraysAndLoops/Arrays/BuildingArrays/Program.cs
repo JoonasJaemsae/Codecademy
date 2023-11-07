2eb4cd6fc19e93fd3894eca10c9ddde9b577ab41
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-07 13:04:57 +0200

    Complete exercise 'BuildingArrays'

---
+++ csharp/5_ArraysAndLoops/Arrays/BuildingArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] summerStrut;
+            summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+
+            int[] ratings;
+            ratings = new int[] { 3, 3, 3, 3, 3, 3, 3, 3 };
+        }
+    }
+}
