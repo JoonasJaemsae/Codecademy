a8641e5e3297661a3e6c00c1d59b44d5088b0f9d
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-07 14:39:44 +0200

    Complete exercise 'DoWhileLoop'

---
+++ csharp/5_ArraysAndLoops/Loops/DoWhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool buttonClick = true;
+            do
+            {
+                Console.WriteLine("BLARRRRRRRRR");
+            } while (!buttonClick);
+            Console.WriteLine("Time for a five minute break.");
+            Console.ReadLine();
+        }
+    }
+}
