2e19dd425de7bc9b60ac812b563506e34cd0e8c6
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-07 16:29:26 +0200

    Complete exercise 'JumpStatements'

---
+++ csharp/5_ArraysAndLoops/Loops/JumpStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The alarm will ring 3 times.
+            bool buttonClick = false;
+            int counter = 0;
+
+            do
+            {
+                Console.WriteLine("BLARRRRR");
+                counter++;
+                if (counter >= 3)
+                {
+                    break;
+                }
+
+            } while (!buttonClick);
+            Console.ReadLine();
+        }
+    }
+}
