1ac78949062aaa49bc57b318cafc4706dd99dc9c
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-08 11:31:19 +0200

    Complete project 'TheObjectOfYourAffection'

---
+++ csharp/6_ClassesAndObjects/TheObjectOfYourAffection/Profile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        // CONSTRUCTORS
+
+        public Profile(string name, int age, string city, string country, string pronouns, string[] hobbies)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = hobbies;
+        }
+
+        public Profile(string name, int age, string city, string country, string pronouns) : this(name, age, city, country,
+        pronouns, new string[] { })
+        {
+        }
+
+        public Profile(string name, int age, string city, string country) : this(name, age, city, country,
+        "they/them", new string[] { })
+        {
+        }
+
+        // METHODS
+
+        public string ViewProfile()
+        {
+            string hobbiesAsString = String.Join(" ", hobbies);
+            string[] list = new string[] { name, age.ToString(), city, country, pronouns, hobbiesAsString };
+            string allInfo = String.Join(" ", list);
+            return allInfo;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ csharp/6_ClassesAndObjects/TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingProfile
+{
+    /*
+    This program can create dating profiles.
+    Leaving out your pronouns or hobbies is possible
+    with the help of constructor overloading.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hobbies = new string[] { "listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns" };
+            string[] hobbies2 = new String[] { "hobby1", "hobby2", "hobby3" };
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him", hobbies);
+            Console.WriteLine(sam.ViewProfile());
+            Profile max = new Profile("Max", 20, "Myyr York", "Finland");
+            max.SetHobbies(hobbies2);
+            Console.WriteLine(max.ViewProfile());
+            Console.ReadLine();
+        }
+    }
+}
