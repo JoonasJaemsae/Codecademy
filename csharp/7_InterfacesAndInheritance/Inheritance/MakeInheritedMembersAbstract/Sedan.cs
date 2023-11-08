078c2b9e72776a2caa24078a20537d2cfc7e2f41
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-08 13:52:43 +0200

    Complete exercise 'MakeInheritedMembersAbstract'

---
+++ csharp/7_InterfacesAndInheritance/Inheritance/MakeInheritedMembersAbstract/Sedan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearnInheritance
+{
+    class Sedan : Vehicle, IAutomobile
+    {
+        public override string Describe()
+        {
+            return $"This {GetType().Name} is moving on {Wheels} wheels at {Speed} km/h, with license plate {LicensePlate}.";
+        }
+
+        public Sedan(double speed) : base(speed)
+        {
+            Wheels = 4;
+        }
+
+    }
+}
---
+++ csharp/7_InterfacesAndInheritance/Inheritance/MakeInheritedMembersAbstract/Truck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LearnInheritance
+{
+    class Truck : Vehicle, IAutomobile
+    {
+        public override string Describe()
+        {
+            return $"This {GetType().Name} is moving on {Wheels} wheels at {Speed} km/h, with license plate {LicensePlate}.";
+        }
+
+        public double Weight
+        { get; }
+
+        public Truck(double speed, double weight) : base(speed)
+        {
+            Weight = weight;
+
+            if (weight < 400)
+            {
+                Wheels = 8;
+            }
+            else
+            {
+                Wheels = 12;
+            }
+        }
+
+    }
+}
