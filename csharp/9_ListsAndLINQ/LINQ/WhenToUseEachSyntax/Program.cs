f9fdcd0984889cb788d77268e0b39b3faf0b58a8
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-09 18:56:27 +0200

    Complete exercise 'WhenToUseEachSyntax'

---
+++ csharp/9_ListsAndLINQ/LINQ/WhenToUseEachSyntax/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            // Method syntax
+            var introuceableHeroes = heroes.Select(h => $"Introducing...{h}!");
+
+            // Query syntax
+            var indicesOfSpace = from h in heroes
+                          where h.Contains(" ")
+                          select h.IndexOf(" ");
+        
+            // Printing...
+            foreach (var hero in introuceableHeroes)
+            {
+                Console.WriteLine(hero);
+            }
+            Console.WriteLine();
+            foreach (var index in indicesOfSpace)
+            {
+                Console.WriteLine(index);
+            }
+            Console.ReadLine();
+        }
+    }
+}
