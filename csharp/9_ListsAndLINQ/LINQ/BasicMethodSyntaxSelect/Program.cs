89d051c4a31f12500dad7bd0d6778547a8440dbe
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-09 18:41:38 +0200

    Complete exercise 'BasicMethodSyntaxSelect'

---
+++ csharp/9_ListsAndLINQ/LINQ/BasicMethodSyntaxSelect/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var lowerHeroesWithC = heroes.Where(h => h.Contains("c"));
+            lowerHeroesWithC = lowerHeroesWithC.Select(h => h.ToLower());
+
+            var sameResult = heroes.Where(h => h.Contains("c"))
+              .Select(h => h.ToLower());
+
+            foreach (string hero in lowerHeroesWithC)
+            {
+                Console.WriteLine(hero);
+            }
+            foreach (string hero in sameResult)
+            {
+                Console.WriteLine(hero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
