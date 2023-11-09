7468a4f6cbe9d1d837b938c37921752f58439488
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-09 19:04:18 +0200

    Complete exercise 'LINQWithOtherCollections'

---
+++ csharp/9_ListsAndLINQ/LINQ/LINQWithOtherCollections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> heroesList = new List<string> { "D. Va", 
+                "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var heroesWithSpecialChars = heroesList
+              .Where(h => h.Contains(".") || h.Contains("7"));
+
+            foreach (string hero in heroesWithSpecialChars)
+            {
+                Console.WriteLine(hero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
