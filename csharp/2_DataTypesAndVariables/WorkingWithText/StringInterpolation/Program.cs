78b1211c0732f998f57a63da2e176daeb9ea4a52
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-13 14:52:27 +0200

    Add StringInterpolation to the correct folder

---
+++ csharp/2_DataTypesAndVariables/WorkingWithText/StringInterpolation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the variables
+            string beginning = "Once upon a time,";
+            string middle = "The kid climbed a tree";
+            string end = "Everyone lived happily ever after.";
+
+            // Interpolate the string and the variables
+            string story = $"{beginning} {middle}. {end}";
+
+            // Print the story to the console 
+            Console.WriteLine(story);
+            Console.ReadLine();
+        }
+    }
+}
