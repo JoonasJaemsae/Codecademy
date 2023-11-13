8cc88e24a861bf132008705a25f377276912a727
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-13 14:53:49 +0200

    Add StringConcatenation to the correct folder

---
+++ csharp/2_DataTypesAndVariables/WorkingWithText/StringConcatenation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringConcatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the variables
+            string beginning = "Once upon a time";
+            string middle = "there was a C# course";
+            string end = "until I finished it. The End.";
+
+            // Concatenate the string and the variables
+            string story = beginning + " " + middle + " " + end;
+
+            // Print the story to the console 
+            Console.WriteLine(story);
+            Console.ReadLine();
+        }
+    }
+}
