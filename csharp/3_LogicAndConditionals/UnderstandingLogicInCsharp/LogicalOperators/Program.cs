0c20503ac013a17740c6d1ba86644bb3f0856c67
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-16 10:56:53 +0200

    Add exercise LogicalOperators to correct location

---
+++ csharp/3_LogicAndConditionals/UnderstandingLogicInCsharp/LogicalOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            The program determines if you can go to the beach
+            with your friend on your vacation or not based on
+            each one of your needs.
+            */
+            bool beach = true;
+            bool hiking = false;
+            bool city = true;
+            bool yourNeeds = (beach && city);
+            bool friendNeeds = (beach || hiking);
+            bool tripDecision = (yourNeeds && friendNeeds);
+            Console.WriteLine(tripDecision);
+            Console.ReadLine();
+        }
+    }
+}
