bb32bc21e003c30e7c67fc81e953e5e6bb14722b
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-11-16 10:07:49 +0200

    Add MoneyMaker to correct location

---
+++ csharp/2_DataTypesAndVariables/MoneyMaker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program converts a numerical amount to coins.
+            int goldValue = 10;
+            int silverValue = 5;
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Which amount would you like to convert?");
+            string input = Console.ReadLine();
+            double value = Math.Floor(Convert.ToDouble(input));
+            Console.WriteLine($"{value} cents is equal to...");
+            double goldCoins = (Math.Floor(value / goldValue));
+            double remainder = (value % goldValue);
+            double silverCoins = (Math.Floor(remainder / silverValue));
+            remainder = (remainder % silverValue);
+            Console.WriteLine("Gold coins: " + goldCoins);
+            Console.WriteLine("Silver coins: " + silverCoins);
+            Console.WriteLine("Bronze coins: " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
