0615903a8a2d40e1bc35342daca8e8dc34cb83b7
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-12-03 20:53:38 +0200

    Add exercise 'WritingAJSONFile'

---
+++ learn_python_3/9_Files/LearnPythonFiles/WritingAJSONFile/script.py
@@ -0,0 +1,10 @@
+import json
+
+data_payload = [
+  {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
+   'follow up': 'But enough talk!'}
+]
+
+# Creates a JSON file
+with open("data.json", "w") as data_json:
+  json.dump(data_payload, data_json)
