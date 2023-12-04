9f0aae84550c7795e9559807b12caf30241fd1f1
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-12-04 13:09:26 +0200

    Add exercise 'InstanceVariables'

---
+++ learn_python_3/10_Classes/IntroductionToClasses/InstanceVariables/script.py
@@ -0,0 +1,9 @@
+class Store:
+  pass
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+# Both objects can have instance variables that are unique to them.
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
