e458e1a0d3ba6e4a2df1cb3071a3166effeb646f
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-12-05 08:49:20 +0200

    Add exercise 'Function Arguments: A Recap'

---
+++ learn_intermediate_python_3/FunctionArguments/FunctionArguments/FunctionArgumentsARecap/script.py
@@ -0,0 +1,19 @@
+tables = {
+  1: ['Jiho', False],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+}
+print(tables)
+
+# Write your code below: 
+def assign_table(table_number, name, vip_status=False):
+  tables[table_number] = [name, vip_status]
+
+assign_table(6, 'Yoni', False)
+assign_table(name='Martha', vip_status=True, table_number=3)
+assign_table(4, 'Karla')
+print(tables)
