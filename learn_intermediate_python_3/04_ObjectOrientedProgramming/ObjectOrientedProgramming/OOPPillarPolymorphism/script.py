555a1b7c2562c3954e545e58ab82335cf81d980f
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-01-01 15:20:10 +0200

    Add exercise 'OOP Pillar: Polymorphism'

---
+++ learn_intermediate_python_3/04_ObjectOrientedProgramming/ObjectOrientedProgramming/OOPPillarPolymorphism/script.py
@@ -0,0 +1,27 @@
+class Employee():
+  new_id = 1
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+
+  def say_id(self):
+    print("My id is {}.".format(self.id))
+
+class Admin(Employee):
+  def say_id(self):
+    super().say_id()
+    print("I am an admin.")
+
+class Manager(Admin):
+  def say_id(self):
+    super().say_id()
+    print("I am in charge!")
+
+# Write your code below
+e1 = Employee()
+e2 = Admin()
+e3 = Manager()
+
+meeting = [e1, e2, e3]
+for obj in meeting:
+  obj.say_id()
