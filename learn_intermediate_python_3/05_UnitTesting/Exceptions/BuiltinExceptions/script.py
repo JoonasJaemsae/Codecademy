08e875f25797a0827f1d24ac674ae83c2b2c75b8
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-01-15 10:41:18 +0200

    Add exercise 'Built-in Exceptions'

---
+++ learn_intermediate_python_3/05_UnitTesting/Exceptions/BuiltinExceptions/script.py
@@ -0,0 +1,8 @@
+sale_instruments = ['Violin', 'Conga', 'Clavinet']
+
+print(TypeError.__bases__)
+
+print('The following ' + str(len(sale_instruments)) + ' instruments are on sale:')
+print(sale_instruments[0])
+print(sale_instruments[1])
+print(sale_instruments[2])
