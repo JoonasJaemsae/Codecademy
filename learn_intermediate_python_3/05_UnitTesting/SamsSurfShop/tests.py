8c6af304a356cb2559b81eca6064d793cdcb0730
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-02-07 09:11:32 +0200

    Add test for adding surfboards

--- learn_intermediate_python_3/05_UnitTesting/SamsSurfShop/tests.py
+++ learn_intermediate_python_3/05_UnitTesting/SamsSurfShop/tests.py
@@ -4,3 +4,11 @@ import unittest
 
 class TestSurfshop(unittest.TestCase):
     pass
+
+    def setUp(self):
+      self.cart = surfshop.ShoppingCart()
+
+    def test_add_surfboards(self):
+      self.assertEqual(self.cart.add_surfboards(1), 'Successfully added 1 surfboard to cart!')
+
+unittest.main()
