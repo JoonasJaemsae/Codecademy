9f4afc4149806bbea0291a12a96b7ea129d8d7b4
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-02-11 08:42:11 +0200

    Change method name

--- learn_intermediate_python_3/05_UnitTesting/SamsSurfShop/tests.py
+++ learn_intermediate_python_3/05_UnitTesting/SamsSurfShop/tests.py
@@ -3,23 +3,22 @@ import surfshop
 import unittest
 
 class TestSurfshop(unittest.TestCase):
-    pass
 
     def setUp(self):
       self.cart = surfshop.ShoppingCart()
 
     def test_add_surfboards(self):
       self.assertEqual(self.cart.add_surfboards(1), 'Successfully added 1 surfboard to cart!')
-    
+
     def test_add_surfboards_2(self):
       self.assertEqual(self.cart.add_surfboards(2), 'Successfully added 2 surfboards to cart!')
 
-    def test_add_surfboards_4(self):
+    def test_add_surfboards_5(self):
       self.assertRaises(surfshop.TooManyBoardsError, self.cart.add_surfboards, 5)
 
     @unittest.expectedFailure
     def test_apply_locals_discount(self):
       self.cart.apply_locals_discount()
       self.assertTrue(self.cart.locals_discount, True)
-        
+      
 unittest.main()
