499d5d734ffa014b021f60f6b0d58eb1bd249864
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-03-09 21:31:40 +0200

    Complete task 3

--- learn_intermediate_python_3/06_IteratorsAndGenerators/NewTeacherInTown/script.py
+++ learn_intermediate_python_3/06_IteratorsAndGenerators/NewTeacherInTown/script.py
@@ -1,8 +1,15 @@
 from roster import student_roster
+from classroom_organizer import ClassroomOrganizer
 
-student_roster_iterator = iter(student_roster)
+# Task 1
+# student_roster_iterator = iter(student_roster)
 
-i = 0
-while i in range(0, 10):
-  print(next(student_roster_iterator))
-  i = i + 1
+# i = 0
+# while i in range(0, 10):
+#   print(next(student_roster_iterator))
+#   i = i + 1
+
+# Task 3
+students = ClassroomOrganizer()
+for student in students:
+  print(student)
