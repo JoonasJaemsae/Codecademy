a8a6e08687db2bd4a934e667c726f69a4a2509df
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-03-15 15:06:29 +0200

    Complete exercise 'Generator Methods Send() for real'

--- learn_intermediate_python_3/06_IteratorsAndGenerators/Generators/GeneratorMethodsSend/script.py
+++ learn_intermediate_python_3/06_IteratorsAndGenerators/Generators/GeneratorMethodsSend/script.py
@@ -8,9 +8,13 @@ def get_student_ids():
     if n is not None:
       student_id = n
       continue
+
     student_id += 1
 
 student_id_generator = get_student_ids()
 for i in student_id_generator:
   # Write your code below
+  if i == 1:
+    i = student_id_generator.send(25)
+
   print(i)
