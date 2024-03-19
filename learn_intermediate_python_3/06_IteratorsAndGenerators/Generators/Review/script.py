f4fb53e771d05d14f0aca851b181f5b6797ac2af
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-03-19 13:26:56 +0200

    Complete grad_days generator

--- learn_intermediate_python_3/06_IteratorsAndGenerators/Generators/Review/script.py
+++ learn_intermediate_python_3/06_IteratorsAndGenerators/Generators/Review/script.py
@@ -16,4 +16,12 @@ def graduation_countdown(days):
 days = 25
 countdown_generator = (day for day in range(days, -1, -1))
 
+grad_days = graduation_countdown(days)
+for day in grad_days:
+  if day == 15:
+    grad_days.send(10)
+  elif day == 3:
+    grad_days.close()
+  print("Days Left: {}".format(day))
+
 gpas = [3.2, 4.0, 3.6, 2.9]
